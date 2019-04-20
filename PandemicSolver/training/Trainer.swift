6f6b90a8c824bef2e93d42a427fd489111c186ac
Joan Coyne <coyne.jo@husky.neu.edu>
2019-04-20 13:56:38 -0400

    running tests

--- PandemicSolver/training/Trainer.swift
+++ PandemicSolver/training/Trainer.swift
@@ -25,13 +25,15 @@ class Trainer
         /**
          Iterating however many times you wanna do this.
         */
-        (0..<100).forEach
+        (0..<20).forEach
         { iteration in
             /**
              This will just run the same game every time, Nick will write something to easily reset it.
              */
+            var maxQ = Float(0.0)
             simulator = simulator.startGame()
             print(simulator.gameStatus)
+            var  count = 0
             while simulator.gameStatus.isInProgress
             {
                 //var updatedState: Simulator
@@ -53,7 +55,7 @@ class Trainer
                 let gameStates = simulator.legalActions().map
                
               { action -> PandemicSimulatorProtocol in
-                    print(action.description)
+                //print(action.description)
                     return try! simulator.execute(action: action)
                 }
                
@@ -61,15 +63,46 @@ class Trainer
                    
                 { (gameState1, gameState2) -> Bool in
                     self.utility.calculateUtilityWithWeights(currentGameState: gameState1, currentWeights: utility.weights)
-                        < utility.calculateUtilityWithWeights(currentGameState: gameState2, currentWeights: utility.weights)
+                        > utility.calculateUtilityWithWeights(currentGameState: gameState2, currentWeights: utility.weights)
                 })
                 //Assuming just greedy
+                let newMax = self.utility.calculateUtilityWithWeights(currentGameState:maxGameState!, currentWeights: utility.weights)
+                //print (newMax)
+              
+                utility.weights = utility.updateWeights(currentGameState: simulator, currentWeights: utility.weights, predictedUtility: newMax, actualUtility:maxQ)
+                //print(newMax)
                 simulator = maxGameState!
-                print (self.utility.calculateUtilityWithWeights(currentGameState:maxGameState!, currentWeights: utility.weights))
+                //print(simulator.cubesRemaining, simulator.curedDiseases, simulator.infectionRate, simulator.location(of: simulator.currentPlayer))
+                count = count + 1
                 
         }
             
             print(simulator.gameStatus)
+            print(count)
+             
+           /* utility.weights["minDistance"] =  utility.weights["minDistance"]! * -0.01
+           utility.weights["cubesOnBoard"] = utility.weights["cubesOnBoard"]! * -0.01
+            utility.weights["cubesRemaining"] = utility.weights["cubesRemaining"]! * -0.01
+            utility.weights["curedDiseases"] = utility.weights["curedDiseases"]! * -0.01
+            utility.weights["maxOutbreaks"] = utility.weights["maxOutbreaks"]! * -0.01
+            utility.weights["playerDeckCount"] = utility.weights["playerDeckCount"]! * -0.01
+            utility.weights["uncuredDiseases"] = utility.weights["uncuredDiseases"]! * -0.01
+            utility.weights["outbreaksSoFar"] = utility.weights["outbreaksSoFar"]! * -0.01
+            utility.weights["playerDeckCount"] = utility.weights["infectionRate"]! * -0.01*/
+            
+            utility.weights["minDistance"] =   -1.0
+             utility.weights["cubesOnBoard"] = -1.0
+             utility.weights["cubesRemaining"] = -1.0
+             utility.weights["curedDiseases"] = 1.0
+             utility.weights["maxOutbreaks"] = -1.0
+             utility.weights["playerDeckCount"] = -1.0
+             utility.weights["uncuredDiseases"] = -1.0
+             utility.weights["outbreaksSoFar"] = -1.0
+             utility.weights["playerDeckCount"] = -1.0
+                
+            
+        
+           
             simulator = simulator.reset()
             //TODO: Calculate distance between two locations
             //Use LocationSearchHelper
